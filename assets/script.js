933368e25f2132801d79792e85bc4d94c6f9fcb2
NoeliaCI <>
2023-09-14 13:56:11 +0000

    Add variables to script file

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+//Variables
+//Home Screen Buttons
+const quizBtn = document.getElementById('quiz-btn');
+const leaderboardBtn = document.getElementById('leaderboard-btn');
+const howToPlayBtn = document.getElementById('how-to-play-btn');
+const homeButtonIcon = document.getElementById('home-button');
+//Quiz Screen
+const startBtn = document.getElementById('start-btn');
+const startQuiz = document.getElementById('start-quiz');
+const answerButton1 = document.getElementById('answer1');
+const answerButton2 = document.getElementById('answer2');
+const answerButton3 = document.getElementById('answer3');
+const answerButton4 = document.getElementById('answer4');
+const quizQuestions = document.getElementById('questions');
+const questionElement = document.getElementsByClassName('question');
+const nextBtn = document.getElementById('next');
+const questionTotal = document.getElementsByClassName('question-total');
+const headerScore = document.getElementsByClassName('header-score');
+//Results Screen
+const tryAgainBtn = document.getElementById('try-again');
+const goHomeResults = document.getElementById('results-go-home');
+//How to play Screen
+const goHomeHowToPlay = document.getElementById('how-to-play-home-btn');
+//Leaderboard Screen
+const goHomeLeaderboard = document.getElementById('leaderboard-home-btn');
+
+
+
